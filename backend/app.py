b5fa703d9b7d6f21c4f3f1156c77dc896539306d
gzlcg23 <gonzalo.cg.23@gmail.com>
2025-10-14 19:54:21 -0600

    cambio total de estructura y nombres

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
-import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import qrcode
 from io import BytesIO
 import base64
-from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})  # Permite CORS para frontend
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
